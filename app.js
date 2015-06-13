74be003900d66ddc2d40e656df956af703cb722d
Deven <devend711@gmail.com>
2015-06-13 01:07:54 -0400

    working on curl

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var dotenv = require('dotenv');
+dotenv.load(); // load .env vars
+var yelpbot = require('./yelpbot');
+ 
+var app = express();
+var port = process.env.PORT || 3000;
+
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+// test route
+app.get('/', function (req, res) { res.status(200).send('Hello world!') });
+ 
+// error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(400).send(err.message);
+});
+ 
+app.listen(port, function () {
+  console.log('Slack bot listening on port ' + port);
+});
+
+app.post('/yelp', yelpbot);
---
+++ yelpbot.js
@@ -0,0 +1,48 @@
+yelp = require("yelp").createClient({
+  consumer_key: process.env.YELP_CONSUMER_KEY, 
+  consumer_secret: process.env.YELP_CONSUMER_SECRET,
+  token: process.env.YELP_TOKEN,
+  token_secret: process.env.YELP_TOKEN_SECRET
+});
+
+msgRegex = new RegExp(/(.+)\snear\s(.+)/);
+sliceSize = 5; // max number of yelp results to randomly select from
+
+searchYelp = function (msg, callback) {
+  // parse a message like 'pizza NEAR boston'
+  try {
+    array = msg.match(msgRegex); // parse the message
+    yelp.search({term: array[1], location: array[2]}, function(error, data) {
+      if (error) {
+        console.log(error);
+      } else {
+        // loop through the businesses
+        businesses = data.businesses.slice(0, sliceSize-1);
+        suggestedBusiness = businesses[Math.floor(Math.random()*businesses.length)];
+        callback(suggestedBusiness);
+      }
+    });
+  } 
+  catch (e) {
+    console.log(e);
+    callback(null);
+  }
+}
+
+module.exports = function (req, res, next) {
+  msgText = req.body.text;
+  yelpResult = searchYelp(msgText, function(suggestedBusiness) {
+    business = suggestedBusiness;
+    console.log(business);
+    if (!business) {
+      botPayload = {
+        text : "No results for '" + msgText + "'...format searches like 'Pizza near Boston'"
+      };
+    } else {
+      botPayload = {
+        text : 'What about ' + '<' + business.url + '|' + business.name + '>' + '?'
+      };
+    }
+    return res.status(200).send(botPayload);
+  });
+}
